774f41039cf3a912747252222f0a09e9923a7f8b
codingARa <74619512+codingARa@users.noreply.github.com>
2021-12-13 14:35:26 +0100

    minimal solution Part1

--- Day02/Day02Code/Program.cs
+++ Day02/Day02Code/Program.cs
@@ -12,12 +12,29 @@ namespace Day02Code
             Console.WriteLine("Let's solve Day 02");
             var inputString = File.ReadAllLines("input.txt")
                 .ToList();
-            int answer = SteerSubmarine(inputString);
+            int answer1 = SteerSubmarine(inputString);
+            Console.WriteLine($"Answer to Part 1: {answer1}");
         }
 
         public static int SteerSubmarine(List<string> directions)
         {
-            return 1;
+            int fwd = 0;
+            int depth = 0;
+            foreach (string direction in directions)
+            {
+                var dir = direction.Split(" ");
+                if (dir[0] == "forward")
+                {
+                    fwd += Int32.Parse(dir[1]);
+                } else if (dir[0] == "up")
+                {
+                    depth -= Int32.Parse(dir[1]);
+                } else if (dir[0] == "down")
+                {
+                    depth += Int32.Parse(dir[1]);
+                } else { throw new Exception("unexpected input argument"); }
+            }
+            return fwd*depth;
         }
     }
 }
