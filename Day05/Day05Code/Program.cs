95f644e3575529ce560f651afc5cd83b9437f40f
codingARa <74619512+codingARa@users.noreply.github.com>
2021-12-14 14:03:13 +0100

    Solution to Day05 Part1

--- Day05/Day05Code/Program.cs
+++ Day05/Day05Code/Program.cs
@@ -2,11 +2,69 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Numerics;
 
 namespace Day05Code {
-    class Program {
+    public class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Let's solve Day05!");
+
+            var inputStrings = File.ReadAllLines("input.txt")
+                .ToList();
+            int answer1 = CountVentsPart1(inputStrings);
+            Console.WriteLine($"answer to part 1: {answer1}");
+        }
+        public static int CountVentsPart1(List<string> inputStrings) {
+            List<List<Vector2>> parsedCoords1 = ParseHorizontalAndVertical(inputStrings);
+            return CountMultipleVentsHorizontalAndVertical(parsedCoords1);
+        }
+        public static List<List<Vector2>> ParseHorizontalAndVertical(List<string> inputStrings) {
+            List<List<Vector2>> answer = new();
+            foreach (var line in inputStrings) {
+                var pairs = line.Replace(" -> ", ",").Split(",").Select(Int32.Parse).ToList();
+                // weed out diagonal entries
+                if (pairs[0] == pairs[2] || pairs[1] == pairs[3]){
+                    answer.Add(
+                        new List<Vector2>() {
+                            // make sure, that the pair in answer goes from
+                            // left to right, or top to bottom
+                            new(Math.Min(pairs[0],pairs[2]), Math.Min(pairs[1], pairs[3])),
+                            new(Math.Max(pairs[0],pairs[2]), Math.Max(pairs[1], pairs[3])),
+                        });
+                }
+            }
+            return answer;
+        }
+        public static int CountMultipleVentsHorizontalAndVertical(List<List<Vector2>> parsedCoords) {
+            List<Vector2> vents = new();
+            Vector2 vertical = new(0, 1);
+            Vector2 horizontal = new(1, 0);
+
+            foreach (var pair in parsedCoords) {
+                // march vertically
+                if (pair[0].X == pair[1].X) {
+                    while (pair[0].Y <= pair[1].Y) {
+                        vents.Add(pair[0]);
+                        pair[0] += vertical;
+                    }
+                }
+                // march horizontally 
+                else {
+                    while (pair[0].X <= pair[1].X) {
+                        vents.Add(pair[0]);
+                        pair[0] += horizontal;
+                    } 
+                }
+
+            }
+
+            // group the list by same coords, only leave behind those which are
+            // counted more than once and count the hole length of the new list
+            int answer = vents
+                .GroupBy(x => x)
+                .Where(g => g.Count() > 1)
+                .Count();
+            return answer;
         }
     }
 }
--- Day05/Day05Test/UnitTest1.cs
+++ Day05/Day05Test/UnitTest1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.IO;
 using Day05Code;
+using System.Numerics;
 
 namespace Day05Test {
     public class Tests {
@@ -13,6 +14,14 @@ namespace Day05Test {
                 .ToList();
             // Count lines, to see that reading file went okay
             Assert.AreEqual(10, inputStrings.Count);
+        }
+
+        [Test]
+        public void Test1_Part1() {
+            var inputStrings = File.ReadAllLines("testInput.txt")
+                .ToList();
+            int answer = Program.CountVentsPart1(inputStrings);
+            Assert.AreEqual(5, answer);
         }
     }
 }
