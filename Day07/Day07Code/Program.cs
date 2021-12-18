3eea3c252d23d7ad374475cf887e329a3040f58d
codingARa <74619512+codingARa@users.noreply.github.com>
2021-12-18 15:03:24 +0100

    inital setup Day07

---
+++ Day07/Day07Code/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+
+namespace Day07Code {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Day07/Day07Test/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using Day07Code;
+using System.Numerics;
+
+namespace Day07Test {
+    public class Tests {
+        [SetUp]
+        public void Setup() {
+        }
+
+        [Test]
+        public void Test1() {
+            Assert.Pass();
+        }
+    }
+}
