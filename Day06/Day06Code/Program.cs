bb7f3a5ccf533f87ebbb5c045a30689c1098878d
codingARa <74619512+codingARa@users.noreply.github.com>
2021-12-18 14:48:39 +0100

    Solution for Part 2 - Needs Refactoring

--- Day06/Day06Code/Program.cs
+++ Day06/Day06Code/Program.cs
@@ -14,6 +14,10 @@ namespace Day06Code {
             Dictionary<int, int> Population = ParseInputPart1(inputStrings[0]);
             int answer1 = CountPopulationAfterNGenerations(Population, 80);
             Console.WriteLine($"answer to part 1: {answer1}");
+
+            Dictionary<int, ulong> Population2 = ParseInputPart2(inputStrings[0]);
+            ulong answer2 = CountPopulationAfterNGenerationsPart2(Population2, 256);
+            Console.WriteLine($"answer to part 2: {answer2}");
         }
         public static Dictionary<int,int> ParseInputPart1(string inputString) {
             Dictionary<int, int> Population = new();
@@ -31,6 +35,39 @@ namespace Day06Code {
         }
         static Dictionary<int, int> GenerationStep(Dictionary<int, int> Population) {
             int nextGen = 0;
+            foreach (var num in Enumerable.Range(0,9)) {
+                if (num == 0) {
+                    nextGen = Population[0];
+                }
+                else {
+                    Population[num - 1] = Population[num];
+                }
+            }
+            Population[6] += nextGen;
+            Population[8] = nextGen;
+            return Population;
+        }
+
+        public static Dictionary<int,ulong> ParseInputPart2(string inputString) {
+            Dictionary<int, ulong> Population = new();
+            foreach(int num in Enumerable.Range(0,9)) {
+                char[] cArray = num.ToString().ToCharArray();
+                Population.Add(num, (ulong)inputString.Count(x => x == cArray[0]));
+            }
+            return Population;
+        }
+        public static ulong CountPopulationAfterNGenerationsPart2(Dictionary<int,ulong> Population, int nGenerations) {
+            for (int n = 0; n < nGenerations; n++) {
+                Population = GenerationStep2(Population);
+            }
+            ulong result = 0;
+            foreach (var num in Enumerable.Range(0, 9)) {
+                result += Population[num];
+            }
+            return result;
+        }
+        static Dictionary<int, ulong> GenerationStep2(Dictionary<int, ulong> Population) {
+            ulong nextGen = 0;
             foreach (var num in Enumerable.Range(0,9)) {
                 if (num == 0) {
                     nextGen = Population[0];
--- Day06/Day06Test/UnitTest1.cs
+++ Day06/Day06Test/UnitTest1.cs
@@ -15,6 +15,12 @@ namespace Day06Test {
             int answer = Program.CountPopulationAfterNGenerations(Population, 80);
             Assert.AreEqual(5934, answer);
         }
+        [Test]
+        public void Test1_Part2() {
+            string initial_state = "3,4,3,1,2";
+            Dictionary<int, ulong> Population = Program.ParseInputPart2(initial_state);
+            ulong answer = Program.CountPopulationAfterNGenerationsPart2(Population, 256);
+            Assert.AreEqual(26984457539, answer);
         }
     }
 }
