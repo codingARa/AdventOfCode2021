0bdec85935fc90b3115549a06040be13f9bc7ab5
codingARa <74619512+codingARa@users.noreply.github.com>
2021-12-28 11:01:29 +0100

    inital setup

---
+++ Day09/Day09Code/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+
+namespace Day09Code {
+    public class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Let's solve Day09!");
+
+            var inputStrings = File.ReadAllLines("input.txt").ToList();
+        }
+    }
+}
---
+++ Day09/Day09Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using Day09Code;
+using System.Numerics;
+
+namespace Day09Test {
+    public class Tests {
+
+        [Test]
+        public void Test1() {
+            Assert.Pass();
+        }
+    }
+}
