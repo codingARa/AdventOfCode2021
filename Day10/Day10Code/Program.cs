c1fad69c237fe93ce36b3ac800a17b8748ca5fae
codingARa <74619512+codingARa@users.noreply.github.com>
2023-02-11 18:41:50 +0100

    minor refactoring on SolutionPart1 + SolutionPart2

--- Day10/Day10Code/Program.cs
+++ Day10/Day10Code/Program.cs
@@ -6,8 +6,12 @@ public class Program
     {
         Console.WriteLine("Let's solve Day10!");
         var input = ParseInput("input.txt");
+
         var answer1 = SolutionPart1(input);
-        Console.WriteLine($"Answer1: {answer1}");
+        Console.WriteLine($"Answer1: {answer1.Item1}");
+
+        var answer2 = SolutionPart2(answer1.Item2);
+        Console.WriteLine($"Answer2: {answer2}");
     }
 
     /// <summary>
@@ -16,7 +20,7 @@ public class Program
     /// </summary>
     /// <param name="input"></param>
     /// <returns>return error score of input file</returns>
-    public static int SolutionPart1(List<string> input)
+    public static (int, List<Stack<char>>) SolutionPart1(List<string> input)
     {
         var scoringDictionary = new Dictionary<char, int> {
             { ')', 3 },
@@ -34,28 +38,75 @@ public class Program
         var openingBrackets = new List<char> { '(', '[', '{', '<' };
 
         int error_score = 0;
+        List<Stack<char>> incompleteLines = new();
 
         foreach (var line in input)
         {
-            Stack<char> q = new();
+            Stack<char> s = new();
+            var currentErrorValue = 0;
             foreach (var currentChar in line)
             {
                 if (openingBrackets.Contains(currentChar)){
-                    q.Push(currentChar);
+                    s.Push(currentChar);
                     continue;
                 }
 
-                var matchingClosingBracket = matchingDictionary[q.Pop()];
+                var matchingClosingBracket = matchingDictionary[s.Pop()];
                 if (currentChar != matchingClosingBracket){
-                    var currentErrorValue = scoringDictionary[currentChar];
+                    currentErrorValue = scoringDictionary[currentChar];
                     error_score += currentErrorValue;
                     break;
                 }
 
             }
+            if (currentErrorValue == 0)
+            {
+                incompleteLines.Add(s);
+            }
         }
 
-        return error_score;
+        return (error_score, incompleteLines);
+    }
+
+    /// <summary>
+    /// taking the list of stacks of unclosed brackets for each line and returning the "middle score"
+    /// </summary>
+    /// <param name="incompleteLines">stack of unclosed brackets from Part1</param>
+    /// <returns>"middle score" compared to every line score</returns>
+    public static ulong SolutionPart2(List<Stack<char>> incompleteLines)
+    {
+        var scores = new List<ulong>();
+        foreach (var stack in incompleteLines)
+        {
+            var remainingOpenBrackets = stack.ToList();
+            var lineScore = EvaluateRemainingBrackets(remainingOpenBrackets);
+            scores.Add(lineScore);
+        }
+        var sortedScores =  scores.OrderBy(i => i).ToList();
+        var middleScore = sortedScores[Convert.ToInt32(scores.Count / 2)];
+        return middleScore;
+    }
+
+    /// <summary>
+    /// evaluate the remaining brackets by the provided algorithm
+    /// </summary>
+    /// <param name="remainingOpenBrackets">list of remaining brackets</param>
+    /// <returns>score for a given list of remaining brackets</returns>
+    private static ulong EvaluateRemainingBrackets(List<char> remainingOpenBrackets)
+    {
+        ulong score = 0;
+        var scoringDictionary = new Dictionary<char, ulong> {
+            { '(', 1 },
+            { '[', 2 },
+            { '{', 3 },
+            { '<', 4 }
+        };
+        foreach (var item in remainingOpenBrackets)
+        {
+            score *= 5;
+            score += scoringDictionary[item];
+        }
+        return score;
     }
 
     /// <summary>
--- Day10/Day10Test/UnitTest1.cs
+++ Day10/Day10Test/UnitTest1.cs
@@ -7,6 +7,15 @@ public class Tests
     {
         var input = Program.ParseInput("testinput.txt");
         var answer1 = Program.SolutionPart1(input);
-        Assert.AreEqual(26397, answer1);
+        Assert.AreEqual(26397, answer1.Item1);
+    }
+
+    [Test]
+    public void TestSolutionPart2()
+    {
+        var input = Program.ParseInput("testinput.txt");
+        var answer1 = Program.SolutionPart1(input);
+        var answer2 = Program.SolutionPart2(answer1.Item2);
+        Assert.AreEqual(288957, answer2);
     }
 }
